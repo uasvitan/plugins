97e4914f6325a15994f381be33c3125e2addfeef
uasvitan <uasvitan@gmail.com>
2026-01-06 17:04:29 +0200

    Add files via upload

---
+++ pubtorr.my.js
@@ -0,0 +1,195 @@
+(function () {
+    'use strict';
+
+    function translate() {
+      Lampa.Lang.add({
+        lme_parser: {
+          ru: 'Каталог парсеров',
+          en: 'Parsers catalog',
+          uk: 'Каталог парсерів',
+          zh: '解析器目录' // Chinese translation
+        },
+        lme_parser_description: {
+          ru: 'Нажмите для выбора парсера из ',
+          en: 'Click to select a parser from the ',
+          uk: 'Натисніть для вибору парсера з ',
+          zh: '单击以从可用的 '
+        },
+        lme_pubtorr: {
+          ru: 'Каталог TorrServer',
+          en: 'TorrServer catalog',
+          uk: 'Каталог TorrServer',
+          zh: '解析器目录' // Chinese translation
+        },
+        lme_pubtorr_description: {
+          ru: 'Бесплатные серверы от проекта LME',
+          en: 'Free servers from the LME project',
+          uk: 'Безкоштовні сервери від проєкту LME',
+          zh: '来自 LME 项目的免费服务器 '
+        },
+        lme_pubtorr_firstrun: {
+          "ru": "Привет! Ты установил плагин LME PubTorr, учти что если стоит Mods's то в разделе парсеров будет ошибка, которая не влияет на работу. Хочешь избавиться - оставь или LME PubTorr или Mods's.",
+          "en": "Hello! You have installed the LME PubTorr plugin. Note that if Mods's is enabled, there will be an error in the parsers section that does not affect functionality. If you want to get rid of it, keep either LME PubTorr or Mods's.",
+          "uk": "Привіт! Ви встановили плагін LME PubTorr, врахуйте, що якщо активовано Mods's, то в розділі парсерів буде помилка, яка не впливає на роботу. Якщо хочете позбутися - залиште або LME PubTorr, або Mods's.",
+          "zh": "你好！你安装了LME PubTorr插件，请注意，如果启用了Mods's，解析器部分将出现错误，但这不会影响功能。如果你想摆脱它，请保留LME PubTorr或Mods's。"
+        }
+      });
+    }  
+    var Lang = {  
+      translate: translate  
+    };  
+  
+    var parsersInfo = [
+      {
+        base: 'jacred.pro',
+        name: 'Jacred.pro',
+        settings: {
+          url: 'jacred.pro',
+          key: '',
+          parser_torrent_type: 'jackett'
+        }
+      },
+      {
+        base: 'jacred_xyz',
+        name: 'Jacred.xyz',
+        settings: {
+          url: 'jacred.xyz',
+          key: '',
+          parser_torrent_type: 'jackett'
+        }
+      },
+      {
+        base: 'https://jr.maxvol.pro',
+        name: 'jr.maxvol.pro',
+        settings: {
+          url: 'https://jr.maxvol.pro',
+          key: '',
+          parser_torrent_type: 'jackett'
+        }
+      },
+      {
+        base: 'http://spawnum.duckdns.org:49117',
+        name: 'Spawn V1 🇺🇦',
+        settings: {
+          url: 'http://spawnum.duckdns.org:49117',
+          key: '',
+          api: 2,
+          parser_torrent_type: 'jackett'
+        }
+      },
+      {
+        base: 'http://spawnum.duckdns.org:59117',
+        name: 'Spawn V2 🇺🇦',
+        settings: {
+          url: 'http://spawnum.duckdns.org:59117',
+          key: '',
+          api: 2,
+          parser_torrent_type: 'jackett'
+        }
+      },
+      {
+        base: 'jac.stull.xyz',
+        name: 'jac.stull.xyz',
+        settings: {
+          url: 'jac.stull.xyz',
+          key: '',
+          api: 1,
+          parser_torrent_type: 'jackett'
+        }
+      },
+      {
+        base: 'https://jackett.1337.cx',
+        name: 'jackett.1337.cx',
+        settings: {
+          url: 'https://jackett.1337.cx',
+          key: '',
+          api: 7,
+          parser_torrent_type: 'jackett'
+        }
+      }
+    ];
+
+    /**/
+    // Lampa.Controller.listener.follow('toggle', (e) => {
+    //     if (e.name === 'select') {
+    //         checkAlive("parser");
+    //     }
+    // });
+
+    function changeParser() {
+      var jackettUrlTwo = Lampa.Storage.get("lme_url_two");
+      var selectedParser = parsersInfo.find(function (parser) {
+        return parser.base === jackettUrlTwo;
+      });
+      if (selectedParser) {
+        var settings = selectedParser.settings;
+        Lampa.Storage.set(settings.parser_torrent_type === 'prowlarr' ? "prowlarr_url" : "jackett_url", settings.url);
+        Lampa.Storage.set(settings.parser_torrent_type === 'prowlarr' ? "prowlarr_key" : "jackett_key", settings.key);
+        Lampa.Storage.set("parser_torrent_type", settings.parser_torrent_type);
+      } else {
+        console.warn("Jackett URL not found in parsersInfo");
+      }
+    }
+    var s_values = parsersInfo.reduce(function (prev, _ref) {
+      var base = _ref.base,
+        name = _ref.name;
+      prev[base] = name;
+      return prev;
+    }, {
+      no_parser: 'Не выбран'
+    });
+    function parserSetting() {
+      Lampa.SettingsApi.addParam({
+        component: 'parser',
+        param: {
+          name: 'lme_url_two',
+          type: 'select',
+          values: s_values,
+          "default": 'no_parser'
+        },
+        field: {
+          name: "<div class=\"settings-folder\" style=\"padding:0!important\"><div style=\"font-size:1.0em\">".concat(Lampa.Lang.translate('lme_parser'), "</div></div>"),
+          description: "".concat(Lampa.Lang.translate('lme_parser_description'), " ").concat(parsersInfo.length)
+        },
+        onChange: function onChange(value) {
+          changeParser();
+          Lampa.Settings.update();
+        },
+        onRender: function onRender(item) {
+          $('.settings-param__value p.parserName').remove();
+          changeParser();
+          setTimeout(function () {
+            $('div[data-children="parser"]').on('hover:enter', function () {
+              Lampa.Settings.update();
+            });
+            if (Lampa.Storage.field('parser_use')) {
+              item.show();
+              $('.settings-param__name', item).css('color', 'f3d900');
+              $('div[data-name="lme_url_two"]').insertAfter('div[data-children="parser"]');
+            } else {
+              item.hide();
+            }
+          });
+        }
+      });
+    }
+    var Parser = {
+      parserSetting: parserSetting
+    };
+
+    Lampa.Platform.tv();
+    function add() {
+      Lang.translate();
+      Parser.parserSetting();
+    }
+    function startPlugin() {
+      window.plugin_lmepublictorr_ready = true;
+      if (window.appready) add();else {
+        Lampa.Listener.follow('app', function (e) {
+          if (e.type === 'ready') add();
+        });
+      }
+    }
+    if (!window.plugin_lmepublictorr_ready) startPlugin();
+
+})();
